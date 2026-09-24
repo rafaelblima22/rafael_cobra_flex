b94b9a5f0c3c164743e442a5948e17b32f6815cd
Rafael <rafaelblima22@gmail.com>
2026-09-23 21:43:22 -0300

    Aula do dia 23/09/2026

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/melhorCombustivel", methods=["POST"])
+def calcular_combustivel():
+    gasolina = None
+    etanol = None
+    result = None
+    if request.method == 'POST':
+        gasolina = float(request.form.get("gasolina","").replace(",","."))
+        etanol = float(request.form.get("etanol","").replace(",","."))
+        result = "Etanol" if etanol <= gasolina * 0.75 else "Gasolina"
+        msg = f"Melhor abastecer com {result}."
+    return render_template("index.html", msg=msg)
+
 if __name__ == "__main__":
     app.run(debug=True)
